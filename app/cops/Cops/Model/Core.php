7310ec97be9d9d2b265441460b737f5821734a67
Mathieu Duplouy <mathieu.duplouy@gmail.com>
2013-12-03 11:46:56 +0100

    Added archive download on serie and author page

--- app/cops/Cops/Model/Core.php
+++ app/cops/Cops/Model/Core.php
@@ -217,6 +217,18 @@ class Core implements CoreInterface
     }
 
     /**
+     * Remove accent from a string
+     */
+    public function removeAccents($str, $charset='utf-8')
+    {
+        $str = htmlentities($str, ENT_NOQUOTES, $charset);
+        $str = preg_replace('#&([A-za-z])(?:acute|cedil|circ|grave|orn|ring|slash|th|tilde|uml);#', '\1', $str);
+        $str = preg_replace('#&([A-za-z]{2})(?:lig);#', '\1', $str);
+        $str = preg_replace('#&[^;]+;#', '', $str);
+        return $str;
+    }
+
+    /**
      * Empty properties on clone
      */
     public function __clone()
