225b5af02310c06f9e8173f3b734eb023050bc50
Mathieu Duplouy <mathieu.duplouy@gmail.com>
2014-01-15 18:04:00 +0100

    Security leak on magic call

--- app/cops/Cops/Model/Common.php
+++ app/cops/Cops/Model/Common.php
@@ -41,10 +41,6 @@ abstract class Common extends Core
      */
     public function __call($method, $args)
     {
-        if (method_exists($this, $method)) {
-            return call_user_func_array(array($this, $method), $args);
-        }
-
         $propKey = $this->_getPropNameFromMethod($method);
 
         switch (substr($method, 0, 3)) {
