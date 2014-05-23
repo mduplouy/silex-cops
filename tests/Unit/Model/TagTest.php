f3e2cd8468715dad4702fa6311564ce2f66e6a69
Mathieu Duplouy <mathieu.duplouy@gmail.com>
2014-05-23 21:36:48 +0200

    Tag unit testing

--- tests/Unit/Model/TagTest.php
+++ tests/Unit/Model/TagTest.php
@@ -26,7 +26,75 @@ class TagTest extends \PHPUnit_Framework_TestCase
         $tag = new \Cops\Model\Tag($this->app);
         $nbBook = $tag->getNumberOfBooks();
         $this->assertEquals(0, $nbBook);
+    }
 
+    /**
+     * @dataProvider getNewTags
+     *
+     * @parma int    $bookId
+     * @param string $tagName
+     */
+    public function testAssociateToBook($bookId, $tagName)
+    {
+        /**
+         * @var Cops\Model\Tag
+         */
+        $tag = $this->app['model.tag'];
+        $this->assertTrue($tag->associateToBook($bookId, $tagName));
+    }
+
+    /**
+     * New tag data provider
+     */
+    public function getNewTags()
+    {
+        return array(
+            array(5, 'test-tag'),
+            array(5, 'another-tag'),
+            array(5, 'last-tag'),
+        );
+    }
+
+    public function testAssociateToBookWithTagObject()
+    {
+        /**
+         * @var Cops\Model\Tag
+         */
+        $tag = $this->app['model.tag'];
+        $tag->setName('dummy-tag');
+
+        $this->assertTrue($tag->associateToBook(5));
+    }
+
+    /**
+     * @depends testAssociateToBookWithTagObject
+     */
+    public function testLoadByNameOk()
+    {
+        /**
+         * @var Cops\Model\Tag
+         */
+        $tag = $this->app['model.tag'];
+        $tag->loadByName('dummy-tag');
+
+        $this->assertEquals($tag->getName(), 'dummy-tag');
+    }
+
+    /**
+     * @depends testLoadByNameOk
+     */
+    public function testDeleteFromBook()
+    {
+        /**
+         * @var Cops\Model\Tag
+         */
+        $tag = $this->app['model.tag'];
+        $this->assertTrue($tag->deleteFromBook(5));
+
+        $book = $this->app['model.book'];
+        $book->load(5);
+
+        $this->assertEquals(0, $book->getTags()->count());
     }
 
 }
