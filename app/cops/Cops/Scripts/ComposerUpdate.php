8b4570c0292638afdbd4b9509221a03d4f904677
Mathieu Duplouy <mathieu.duplouy@gmail.com>
2014-09-21 20:44:00 +0200

    Better error handling on composer update script command

--- app/cops/Cops/Scripts/ComposerUpdate.php
+++ app/cops/Cops/Scripts/ComposerUpdate.php
@@ -12,7 +12,7 @@ namespace Cops\Scripts;
 use Composer\Script\Event;
 use Silex\Application;
 use Symfony\Component\Console\Input\ArrayInput;
-
+use Symfony\Component\Console\Output\ConsoleOutput;
 /**
  * Composer update script
  * @author Mathieu Duplouy <mathieu.duplouy@gmail.com>
@@ -44,8 +44,41 @@ class ComposerUpdate
      */
     private static function databaseUpdate()
     {
-        if (!file_exists(BASE_DIR . self::$app['config']->getValue('internal_db'))) {
-            self::$app['model.user']->getResource()->initTable();
+        $targetDB = BASE_DIR . self::$app['config']->getValue('internal_db');
+        $targetDir = dirname($targetDB);
+
+        if (!file_exists($targetDB)) {
+            if (!is_dir($targetDir)) {
+                @mkdir($targetDir, 0755, true);
+            }
+
+            $output = new ConsoleOutput;
+            $output->writeln('');
+
+            if (is_dir($targetDir)) {
+                self::$app['model.user']->getResource()->initTable();
+
+                $output->writeln('<info>Internal database setup : Done !</info>');
+                $output->writeln(
+                    sprintf(
+                        '<info>Admin login set to %s : %s</info>',
+                        self::$app['config']->getValue('default_login'),
+                        self::$app['config']->getValue('default_password')
+                    )
+                );
+                $output->writeln('');
+
+                return;
+            }
+
+            $output->writeln(
+                sprintf(
+                    '<error> Unable to create directory %s, please check permissions, then run "composer run-script post-update-cmd" command </error>',
+                    realpath($targetDB)
+                )
+            );
+            $output->writeln('');
+
         }
     }
 }
